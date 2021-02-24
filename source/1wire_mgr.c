aaed58d5088c15f06aa54fbe76879157144bee22
Dawid Babula <dbabula@adventurous.pl>
2021-02-24 22:18:58 +0100

    Add CRC checking of the scratchpad space

--- source/1wire_mgr.c
+++ source/1wire_mgr.c
@@ -29,6 +29,7 @@
 #include "system.h"
 #include "debug.h"
 #include "system_timer.h"
+#include <stddef.h>
 
 typedef enum
 {
@@ -37,20 +38,75 @@ typedef enum
     READ_CONVERSION_RESULT,
 } WIRE_state_t;
 
+typedef union
+{
+    struct
+    {
+        uint8_t temp_lsb;
+        uint8_t temp_msb;
+        uint8_t th;
+        uint8_t tl;
+        uint8_t config;
+        uint8_t reserved1;
+        uint8_t reserved2;
+        uint8_t reserved3;
+        uint8_t crc;
+    };
+    uint8_t raw[9];
+} WIRE_scratchpad_space_t;
+
 static WIRE_state_t state;
 static uint16_t temperature;
+static WIRE_scratchpad_space_t scratchpad;
+
+static uint8_t calc_crc(uint8_t crc, uint8_t data)
+{
+    // https://www.maximintegrated.com/en/app-notes/index.mvp/id/27
+    static const uint8_t table[256] = {
+            0, 94, 188, 226, 97, 63, 221, 131, 194, 156, 126, 32, 163, 253, 31, 65,
+            157, 195, 33, 127, 252, 162, 64, 30, 95, 1, 227, 189, 62, 96, 130, 220,
+            35, 125, 159, 193, 66, 28, 254, 160, 225, 191, 93, 3, 128, 222, 60, 98,
+            190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,
+            70, 24, 250, 164, 39, 121, 155, 197, 132, 218, 56, 102, 229, 187, 89, 7,
+            219, 133, 103, 57, 186, 228, 6, 88, 25, 71, 165, 251, 120, 38, 196, 154,
+            101, 59, 217, 135, 4, 90, 184, 230, 167, 249, 27, 69, 198, 152, 122, 36,
+            248, 166, 68, 26, 153, 199, 37, 123, 58, 100, 134, 216, 91, 5, 231, 185,
+            140, 210, 48, 110, 237, 179, 81, 15, 78, 16, 242, 172, 47, 113, 147, 205,
+            17, 79, 173, 243, 112, 46, 204, 146, 211, 141, 111, 49, 178, 236, 14, 80,
+            175, 241, 19, 77, 206, 144, 114, 44, 109, 51, 209, 143, 12, 82, 176, 238,
+            50, 108, 142, 208, 83, 13, 239, 177, 240, 174, 76, 18, 145, 207, 45, 115,
+            202, 148, 118, 40, 171, 245, 23, 73, 8, 86, 180, 234, 105, 55, 213, 139,
+            87, 9, 235, 181, 54, 104, 138, 212, 149, 203, 41, 119, 244, 170, 72, 22,
+            233, 183, 85, 11, 136, 214, 52, 106, 43, 117, 151, 201, 74, 20, 246, 168,
+            116, 42, 200, 150, 21, 75, 169, 247, 182, 232, 10, 84, 215, 137, 107, 53
+    };
+
+    return table[crc ^ data];
+}
+
+static uint8_t calc_crc_block(uint8_t crc, const uint8_t * buffer, size_t len)
+{
+    do
+    {
+        crc = calc_crc(crc, *buffer++);
+        DEBUG(DL_VERBOSE, "buffer 0x%02x, crc 0x%02x, len %d\n", (uint8_t)*(buffer - 1),
+                crc, len);
+    }
+    while (--len > 0);
+    return crc;
+}
 
 static void wire_mgr_main(void)
 {
     static uint32_t time = 0u;
 
-    DEBUG(DL_DEBUG, "Status %d\n", state);
+    DEBUG(DL_ERROR, "State %d\n", state);
     switch(state)
     {
         case START_CONVERSION:
             if(!WIRE_reset())
             {
-                DEBUG(DL_DEBUG, "%s", "No Presence\n");
+                DEBUG(DL_WARNING, "%s", "No Presence\n");
             }
             else
             {
@@ -73,11 +129,28 @@ static void wire_mgr_main(void)
                 WIRE_send_byte(0xCC);
                 WIRE_send_byte(0xBE);
 
-                uint8_t lsb = WIRE_read_byte();
-                uint8_t msb = WIRE_read_byte();
-                temperature = (msb << 8U)|lsb;
+                for(uint32_t i = 0u; i < 9u; i++)
+                {
+                    scratchpad.raw[i] = WIRE_read_byte();
+                }
+
+                DEBUG_DUMP(DL_DEBUG, scratchpad.raw,
+                        sizeof(scratchpad.raw)/sizeof(scratchpad.raw[0]));
+
+                uint8_t crc = calc_crc_block(0U, scratchpad.raw, 8u);
+
+                if(crc == scratchpad.crc)
+                {
+                    temperature = (scratchpad.temp_msb << 8U)|scratchpad.temp_lsb;
+                    DEBUG(DL_INFO, "1WIRE: %x\n", temperature);
+                }
+                else
+                {
+                    DEBUG(DL_ERROR, "CRC error exp 0x%02x calc 0x%02x\n",
+                            scratchpad.crc, crc);
+                }
+
                 state = START_CONVERSION;
-                DEBUG(DL_INFO, "1WIRE: %x\n", temperature);
             }
             break;
     }
